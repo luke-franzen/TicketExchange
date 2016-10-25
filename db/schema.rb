60ff4be71e3dffcd19f6d9f1937e24cc5a1815ce
Luke Franzen <luke.franzen@gmail.com>
2016-10-25 16:46:54 +0000

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161024224331) do
+
+  create_table "tickets", force: :cascade do |t|
+  end
+
+  create_table "users", force: :cascade do |t|
+  end
 
 end
