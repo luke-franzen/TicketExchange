8b86ba8125bff5a658e773ac4f00fb16720d15b5
Luka Zdilar <luka-zdilar@uiowa.edu>
2016-10-26 21:20:17 +0000

    Add session and convert signup/login forms to ruby

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
     def create
         @user = User.new(user_params)
         if @user.save
