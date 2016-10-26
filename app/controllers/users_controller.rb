ee89725640879834f4370a151f4667018c2f1646
Luka Zdilar <luka-zdilar@uiowa.edu>
2016-10-26 20:31:01 +0000

    Add controller logic for signing in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Sign up successful."
+            redirect_to welcome_path
+        else
+            render 'new'
+        end
     end
 end
