379d3df2700b3dd383f706db9994cfa9dffbfcf9
Luke Franzen <luke.franzen@gmail.com>
2016-10-26 00:06:46 +0000

    added users and tix migration

--- db/migrate/20161024223129_create_tickets.rb
+++ db/migrate/20161024223129_create_tickets.rb
@@ -1,6 +1,13 @@
 class CreateTickets < ActiveRecord::Migration
   def change
     create_table :tickets do |t|
+      t.string :game
+      t.date :game_date
+      t.int :price
+      t.string :type
+      
+      t.timestamps
+      
     end
   end
 end
--- db/migrate/20161024224331_create_users.rb
+++ db/migrate/20161024224331_create_users.rb
@@ -1,6 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :user_id
+      t.string :email
+      t.string :password
+      t.float :rating
+      t.string :session_token 
+      
+      t.timestamps
     end
   end
 end
