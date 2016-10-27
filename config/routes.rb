41ad2e6788ebd11dc47da2512855fc79ce6e3bbe
Luke Franzen <luke.franzen@gmail.com>
2016-10-27 20:42:56 +0000

    sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   resources :games
 
   # Example of regular route:
