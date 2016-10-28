4c827f6ad453a9c22de9fee6b5866c657f9f41db
Luka Zdilar <luka-zdilar@uiowa.edu>
2016-10-28 00:09:07 +0000

    Fix redundant validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ActiveRecord::Base
     validates :last_name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
-    validates :password, presence: true, length: { minimum: 6 }
-    validates :password_confirmation, presence: true
+    validates :password, length: { minimum: 6 }
 
 end
