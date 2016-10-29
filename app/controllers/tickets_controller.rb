969957842252f9a34b55c9d4a58a395999f36225
Luke Franzen <luke.franzen@gmail.com>
2016-10-29 19:39:12 +0000

    added buy button

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,9 +2,5 @@ class TicketsController < ApplicationController
     def index
         @game = Game.find_by_id(params[:game_id])
         @tickets = @game.tickets
-        @tickets.each do |ticket|
-            user = User.find_by_id(ticket.user_id)
-            puts(user.first_name)
-        end
     end
 end
