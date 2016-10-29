4459711cb6243a2e786b5a82a7d98ab0c9f067bd
Luke Franzen <luke.franzen@gmail.com>
2016-10-29 19:00:09 +0000

    ticket tables added

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,10 @@
 class TicketsController < ApplicationController
-    
     def index
+        @game = Game.find_by_id(params[:game_id])
+        @tickets = @game.tickets
+        @tickets.each do |ticket|
+            user = User.find_by_id(ticket.user_id)
+            puts(user.first_name)
+        end
     end
-    
 end
