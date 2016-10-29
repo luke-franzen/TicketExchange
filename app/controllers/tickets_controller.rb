5620e6a20b74c9846e5b505b0b5f8f8754ded0bd
Luke Franzen <luke.franzen@gmail.com>
2016-10-29 17:33:03 +0000

    hyperlink game names

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,6 @@
+class TicketsController < ApplicationController
+    
+    def index
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  resources :games
-  resources :tickets
-
+  resources :games do 
+    resources :tickets
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
