3581c3a111ddafe1989e615e66b127f06b87aeb8
Luka Zdilar <luka-zdilar@uiowa.edu>
2016-11-02 21:24:31 +0000

    Add more style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
             render 'new'
         end
     end
-    
+
     private
         def user_params
             params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
