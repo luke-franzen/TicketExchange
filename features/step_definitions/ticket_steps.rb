f670acefb23e2b045d565fb0ee43d7003c8d0e43
Luke Franzen <luke.franzen@gmail.com>
2016-11-03 00:10:10 +0000

    fix bug in ticket_steps.rb

--- features/step_definitions/ticket_steps.rb
+++ features/step_definitions/ticket_steps.rb
@@ -1,7 +1,7 @@
 # Completed step definition for tickets_view.feature
 
 Given /^I am on the homepage of the TicketExchange site$/ do
-  visit root_path
+  visit welcome_index_path
  end
 
  When /^I am not logged in$/ do
@@ -9,13 +9,18 @@ Given /^I am on the homepage of the TicketExchange site$/ do
     loggedin = page.find_by_id("login-link")
   rescue Capybara::ElementNotFound
     click_link("logout-link")
+    visit welcome_index_path
   end
  end
 
  And /^I click "Iowa State Cyclones"$/ do
+    #print page.html                              None of these tests work, because the code in the container "quickTickets" from index.html.erb view isn't generated when this is tested
     #click_on("gameid_2")
+    #click_link('gameid_2')
     #click_link "Iowa State Cyclones"
     #page.find("#gameid_2").click
+    #click_on "Iowa State Cyclones"
+    #click_link("Iowa State Cyclones")
  end
 
  Then /^I should see all of the tickets on sale for the Iowa State Game$/ do
