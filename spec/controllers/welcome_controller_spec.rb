29e5e2f8663780d75c7bb89a7f10e86bdad5596a
Ryan Wedoff <ryan-wedoff@uiowa.edu>
2016-11-04 04:10:37 +0000

    UI fixes

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -23,11 +23,13 @@ describe WelcomeController do
         end
         
         it 'should redirect to game page if game is found' do
-           allow(Game).to receive(:find_by_name).with("Miami RedHawks").and_return(1)
-           allow(@game).to receive(:id).and_return(1)
-           post :search_game, {"search_game" => "Miami RedHawks"}
+            
+            game = instance_double("Game",  :name => "Miami RedHawks", :id => 1)
+            allow(Game).to receive(:find_by_name).with("Miami RedHawks").and_return(game)
+            allow(game).to receive(:id).and_return(1)
+            post :search_game, {"search_game" => "Miami RedHawks"}
            
-           expect(subject).to redirect_to(game_tickets_path)
+            expect(response).to redirect_to(game_tickets_path(game.id))
         end
         
         
