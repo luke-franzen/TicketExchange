5cedf01a31c637a8e277f80c5833b24832b5e8ee
Ryan Wedoff <ryan-wedoff@uiowa.edu>
2016-11-04 02:55:50 +0000

    Temp

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -24,7 +24,7 @@ describe WelcomeController do
         
         it 'should redirect to game page if game is found' do
            allow(Game).to receive(:find_by_name).with("Miami RedHawks").and_return(1)
-           allow(@game).to receive(:i).and_return(1)
+           allow(@game).to receive(:id).and_return(1)
            post :search_game, {"search_game" => "Miami RedHawks"}
            
            expect(subject).to redirect_to(welcome_index_path)
