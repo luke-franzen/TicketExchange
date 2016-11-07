0e05765d7bd98ff7a699de575320aa90cc6443bc
Ryan Wedoff <ryan-wedoff@uiowa.edu>
2016-11-07 01:44:13 +0000

    One more test done

---
+++ spec/controllers/games_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe GamesController do
+    describe 'Starting auto search' do
+         it 'should start auto search' do
+        end
+    end
+end
