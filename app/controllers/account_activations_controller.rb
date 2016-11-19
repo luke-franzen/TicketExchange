4bc53d1a0788babc96ec92b05a206837d0dfc42a
Luka Zdilar <luka-zdilar@uiowa.edu>
2016-11-19 06:39:10 +0000

    Fix email verification to flash messages correctly

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,10 +3,10 @@ class AccountActivationsController < ApplicationController
         user = User.find_by(email: params[:email])
         if user && !user.activated? && user.authenticated?(:activation, params[:id])
             user.activate
-            flash[:success] = "Account activated!"
+            flash[:notice] = "Account activated!"
             redirect_to root_url
         else
-            flash[:danger] = "Invalid activation link"
+            flash[:notice] = "Invalid activation link"
             redirect_to root_url
         end
     end
