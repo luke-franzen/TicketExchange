bc88de4073b80ea17e71029213c4c1fb878e01fc
Luke Franzen <luke.franzen@gmail.com>
2016-11-20 18:15:54 +0000

    users rspec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe UsersController do
+     before(:each) do
+        @current_user = User.new({ :id => 1, :first_name => "John", :last_name => "Smith", :email => "jsmith@gmail.com"})
+        @ticket = Ticket.new({:id => 1, :user_id => 1, :game_id => 1, :price => 40, :sold => true, :ticket_type => "student"})
+        allow(Ticket).to receive(:find_by_id).with("1").and_return(@ticket)
+        controller.instance_variable_set(:@current_user, @current_user)
+    end
+
     describe 'Signing Up' do
         it 'should show the sign up page' do
             expect(User).to receive(:new)
@@ -18,6 +25,10 @@ describe UsersController do
         it 'should redirect to the welcome page' do
             post :create, {:user => {first_name: "John", last_name: "Smith", email: "johnsmith@email.com", password: "123456", password_confirmation: "123456" }}
             expect(response).to redirect_to(welcome_index_path)
+        end
+        it 'should show users profile page' do
+            allow(User).to receive(:find).with("1").and_return(@current_user)
+            get :show, {:id => "1"}
         end
     end
 end
