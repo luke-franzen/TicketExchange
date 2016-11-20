5ab27e8bbe9dd4defeb4701a14c0dae2a595bb51
Luke Franzen <luke.franzen@gmail.com>
2016-11-20 18:06:33 +0000

    fixed users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,15 @@ class UsersController < ApplicationController
             render 'new'
         end
     end
+    
+     def show
+        id = params[:id] # retrieve movie ID from URI route
+        @user = User.find(id) # look up movie by unique ID
+        @tickets = @user.tickets.select{|t|!t.sold}
+        if @user == @current_user
+            @sold = @user.tickets.select{|t|t.sold}
+        end
+    end
 
     private
         def user_params
