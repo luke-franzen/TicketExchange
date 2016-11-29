56b8a1307488995d2ce59752c85ed443f6a1a4fd
Luke Franzen <luke.franzen@gmail.com>
2016-11-29 02:04:10 +0000

    need to fix games/delete

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,15 @@
 class GamesController < ApplicationController
     autocomplete :game, :name, :full => true
+    
+    def destroy
+        game = Game.find_by_id(params[:id])
+        #game.tickets.destroy
+        flash[:notice] = game.name+" successfully deleted."
+        redirect_to welcome_index_path
+    end
+    
+    private
+        def game_params
+        params.require(:id)
+        end
 end
