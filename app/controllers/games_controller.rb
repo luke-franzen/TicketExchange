d7404793ef6728af5a7b6c3acf60f8169568b25d
Luke Franzen <luke.franzen@gmail.com>
2016-11-30 02:31:47 +0000

    admin add game functionality

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
 class GamesController < ApplicationController
+    before_filter :set_current_user
     autocomplete :game, :name, :full => true
     
     def destroy
@@ -6,13 +7,29 @@ class GamesController < ApplicationController
         game.tickets.each do |ticket|
             ticket.delete
         end
-        game.destroy    
+        game.destroy
         flash[:notice] = game.name+" successfully deleted."
         redirect_to welcome_index_path
     end
     
+    def new
+        @game = Game.new
+    end
+    
+    def create
+        @game = Game.new(game_params)
+        if @game.save
+            flash[:notice] = @game.name+" successfully added."
+            redirect_to welcome_index_path
+        else
+            flash[:notice] = "Invalid input parameters."
+            redirect_to new_game_path
+        end
+        
+    end
+    
     private
         def game_params
-        params.require(:id)
+            params.require(:game).permit(:name,:date)
         end
 end
