7bf79b345c626215f5280418ef655b9863a4ebd5
Luke Franzen <luke.franzen@gmail.com>
2016-11-30 18:19:40 +0000

    updated games controller tests

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -12,18 +12,36 @@ RSpec.describe GamesController, type: :controller do
     end
 
     describe 'Games controller should' do
+        it 'should create new game correctly' do
+            expect(Game).to receive(:new)
+            get :new
+        end
+        
         it 'successfully delete a game' do
             expect(Game).to receive(:find_by_id).with("1").and_return(@game)
-            allow(@game).to receive(:tickets).and_return(Ticket.all)
-            allow(Ticket.all).to receive(:each).and_yield(@ticket)
-            allow(@ticket).to receive(:delete)
+            expect(@game).to receive(:tickets).and_return(Ticket.all)
+            allow(Ticket.all).to receive(:each).and_return(@ticket)
+            allow(Ticket.all).to receive(:delete)
             expect(@game).to receive(:destroy)
             delete :destroy, {:id => "1"}
             expect(flash[:notice]).to eq "Iowa State Cyclones successfully deleted."
             expect(response).to redirect_to(welcome_index_path)
         end
         
-        it 'successfully add a game' do
+        it 'successfully create a game' do
+            allow(Game).to receive(:new).and_return(@game)
+            expect(@game).to receive(:save).and_return(true)
+            post :create, {:game => {:name => "Iowa State", :date => "22-Nov-2016"}}
+            expect(flash[:notice]).to eq "Iowa State Cyclones successfully added."
+            expect(response).to redirect_to(welcome_index_path)
+        end
+        
+        it 'should successfully guard against invalid game params' do
+            allow(Game).to receive(:new).and_return(@game)
+            expect(@game).to receive(:save).and_return(false)
+            post :create, {:game => {:date => "22-Nov-2016"}}
+            expect(flash[:notice]).to eq "Invalid input parameters."
+            expect(response).to redirect_to(new_game_path)
         end
     end
 end
