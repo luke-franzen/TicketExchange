194d23f3340ee016f35b6bff8125d1d3b3d3ac40
Ryan Wedoff <ryan-wedoff@uiowa.edu>
2016-12-01 04:27:05 +0000

    test

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -51,8 +51,8 @@ def create
   redirect_to conversation_messages_path(@conversation)
  else
  @message = @conversation.messages.new(message_params)
- @message.save
  @message.send_email
+ @message.save
  redirect_to conversation_messages_path(@conversation)
   
  end
--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,7 +1,8 @@
 class MessageMailer < ApplicationMailer
     def new_message(user)
         @user = user
-        puts user
+        puts "HERE"
+        puts user.id
         mail to: user.email, subject: "New Message"
     end
 end
