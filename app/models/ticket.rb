509d3937cfae9a611ff6f0f1b7aa43e04a89d0f4
ElaineMou <elaine.g.mou@gmail.com>
2016-12-01 08:50:58 +0000

    added section row seat

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,7 +2,8 @@ class Ticket < ActiveRecord::Base
    belongs_to :user
    belongs_to :game
 
-   validates :price, presence:true, numericality: { greater_than: 0 }, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }
+   validates :price, presence:true, numericality: { greater_than: 0 }, 
+             :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }
    
    def self.all_types
     ["Student", "General Public", "UI Faculty and Staff", "UI Student Guest"]
